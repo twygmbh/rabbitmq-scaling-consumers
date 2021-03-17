1ad623f6714a20ce281cfe56c8fa4d7b8876e793
Reto Ryter <reto@twy.gmbh>
2021-03-17 17:53:20 +0100

    chore(statefulset): distribute private keys

--- consumer/consumer.js
+++ consumer/consumer.js
@@ -1,7 +1,15 @@
 const amqp = require("amqplib");
 
 const messageQueueConnectionString = process.env.AMQP_URL;
+
 console.log(process.env.AMQP_URL);
+console.log(process.env.POD_NAME);
+
+const parts = process.env.POD_NAME.split("-");
+const nodeNumber = parts[parts.length - 1];
+
+const private_key = process.env["KEY_" + nodeNumber];
+
 const rabbitMqConnection = amqp.connect(messageQueueConnectionString);
 const queue = "create-erc-725-profile";
 
@@ -9,6 +17,7 @@ rabbitMqConnection
   .then((connection) => connection.createChannel())
   .then((channel) => {
     return channel.assertQueue(queue).then(function (ok) {
+      channel.prefetch(2);
       return channel.consume(queue, function (msg) {
         if (msg !== null) {
           console.log(" [x] Received %s", msg.content.toString());
