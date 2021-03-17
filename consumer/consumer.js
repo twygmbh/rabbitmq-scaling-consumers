5e20f740030bcc5d410385feb21090e08328651a
Reto Ryter <reto@twy.gmbh>
2021-03-17 13:39:54 +0100

    chore(setup): chart for dummy relayer

--- consumer/consumer.js
+++ consumer/consumer.js
@@ -1,21 +1,23 @@
 const amqp = require("amqplib");
 
 const messageQueueConnectionString = process.env.AMQP_URL;
+console.log(process.env.AMQP_URL);
 const rabbitMqConnection = amqp.connect(messageQueueConnectionString);
 const queue = "create-erc-725-profile";
 
 rabbitMqConnection
   .then((connection) => connection.createChannel())
-  .then((channel) => channel.assertQueue(queue))
-  .then((ok) => {
-    return channel.consume(queue, (msg) => {
-      if (msg !== null) {
-        console.log(" [x] Received %s", msg.content.toString());
-        setTimeout(function () {
-          console.log(" [x] Done");
-          channel.ack(msg);
-        }, 10 * 1000);
-      }
+  .then((channel) => {
+    return channel.assertQueue(queue).then(function (ok) {
+      return channel.consume(queue, function (msg) {
+        if (msg !== null) {
+          console.log(" [x] Received %s", msg.content.toString());
+          setTimeout(function () {
+            console.log(" [x] Done");
+            channel.ack(msg);
+          }, 10 * 1000);
+        }
+      });
     });
   })
   .catch(console.warn);
